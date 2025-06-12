53266a6b4f17d1988910b6cb960e31880c5d02fd
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-06-12 09:58:00 +0800

    ok: ^wip: and ^debug: can bypass debug log check and my name check

--- hooks/ensure-no-debug-log-or-my-name.commit-msg.sh
+++ hooks/ensure-no-debug-log-or-my-name.commit-msg.sh
@@ -18,6 +18,11 @@ if cat $1 | grep -q "^debug:"; then
     exit 0
 fi
 
+if cat $1 | grep -q "^wip:"; then
+    echo -e "\033[32mskipped ensure-no-debug-log-or-my-name: work in progress\033[0m"
+    exit 0
+fi
+
 should_block_commit=0
 for file in $STAGED_FILES; do
   for word in "${DENIED_WORDS[@]}"; do
