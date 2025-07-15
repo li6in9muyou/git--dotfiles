b451da9b8a9cd8b8bedd29d56ef49e18624e0b8d
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-07-15 17:10:00 +0800

    ok: add a ./.fuck-husky file to skip husky checks

--- hooks/ensure-husky-runs.sh
+++ hooks/ensure-husky-runs.sh
@@ -9,9 +9,14 @@ husky_script="./.husky/$hook_name"
 # Check if the script exists and source it
 if [ -f "$husky_script" ]; then
   echo "#   git--dotfiles/hooks/ensure-husky-runs/$hook_name"
-  . "$husky_script"
-  if [ $? -ne 0 ]; then
-    echo -e "\033[31mAborted: husky failed\033[0m"
-    exit 1
+
+  if [ -f "./.fuck-husky" ]; then
+    echo -e "\033[32mFUCK HUSKY\033[0m"
+  else
+    . "$husky_script"
+    if [ $? -ne 0 ]; then
+      echo -e "\033[31mAborted: husky failed\033[0m"
+      exit 1
+    fi
   fi
 fi
